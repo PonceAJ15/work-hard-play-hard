a334fa7225d1dafdeee9926abc6be80956a3f7cc
PonceAJ15 <ponceaj15@uww.edu>
2022-03-18 17:54:45 -0500

    Changed JSON parser and removed unused classes
    Changed JSON parser
    
    Added dependency org.json for parsing packet definitions. Modified
    PacketInterfaceFactory to use new JSONParser. I don't know why I wasted
    my time writing a custom JSON parser, org.json makes way more sense.
    
    Removed unused classes
    
    Removed HashContext class, Table class, custom implementation of
    JSONObject class.

--- W2HP Packet Translator/src/packet/PacketInterfaceFactory.java
+++ W2HP Packet Translator/src/packet/PacketInterfaceFactory.java
@@ -1,14 +1,16 @@
 package packet;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import parser.JSONObject;
-import parser.JSONObject.JSONField;
-import parser.JSONObject.JSONFieldType;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import util.FieldLabel;
 import util.FieldLabelList;
 import util.Type;
@@ -27,11 +29,12 @@ public final class PacketInterfaceFactory
 	private final HashMap<String, HashMap<String, List<PacketDataSlot>>> PACKET_DEFINITIONS = 
 			  new HashMap<String, HashMap<String, List<PacketDataSlot>>>();
 	
-	public PacketInterfaceFactory(String packetDefinitionPath) throws FileNotFoundException, ParseException
+	public PacketInterfaceFactory(String packetDefinitionPath) throws IOException, ParseException
 	{
-		JSONObject definition = new JSONObject(packetDefinitionPath);
-		checkTypes(definition.getField("data-types"));
-		translatePacketDefinitions(definition.getField("packets"));
+		String content = new String(Files.readAllBytes(Paths.get(packetDefinitionPath)));
+		JSONObject definition = new JSONObject(content);
+		checkTypes(definition.getJSONArray("data-types"));
+		translatePacketDefinitions(definition.getJSONArray("packets"));
 	}
 	
 	/**
@@ -52,46 +55,51 @@ public final class PacketInterfaceFactory
 	 * this function checks that the size of these types are consistent with
 	 * assumptions made by the PackectInterfaceFactory class.
 	 */
-	private void checkTypes(JSONField typeList) throws ParseException
+	private void checkTypes(JSONArray typeList) throws ParseException
 	{
-		if(typeList.TYPE != JSONFieldType.LIST)
-			throw new ParseException("Type definitions expected in LIST type, but JSONField was "+typeList.TYPE+" instead.",0);
-		for(JSONField type: typeList)
-			if(type.getField("size").TYPE == JSONFieldType.OBJECT)
-				if(Type.TYPE_SIZE_ASSUMPTIONS.get(type.getField("type").string()) != null)
-					throw new ParseException("Type "+type.getField("type").string()+" is inconsistent with type assumptions.",0);
-				else if(!type.getField("size").getField("type").string().contentEquals("byte")) //"byte" != "byte"?
-					throw new ParseException("Type "+type.getField("type").string()+" is inconsistent with type assumptions.",0);
+		for(Object typeObject: typeList)
+		{
+			JSONObject type = (JSONObject)typeObject;
+			if(type.get("size") instanceof JSONObject)
+				if(Type.TYPE_SIZE_ASSUMPTIONS.get(type.getString("type")) != null)
+					throw new ParseException("Type "+type.getString("type")+" is inconsistent with type assumptions.",0);
+				else if(!type.getJSONObject("size").getString("type").contentEquals("byte")) //"byte" != "byte"?
+					throw new ParseException("Type "+type.getString("type")+" is inconsistent with type assumptions.",0);
 				else;
-			else if(Type.TYPE_SIZE_ASSUMPTIONS.get(type.getField("type").string()) == null)
-				throw new ParseException("Type "+type.getField("type").string()+" is inconsistent with type assumptions.",0);
-			else if(Type.TYPE_SIZE_ASSUMPTIONS.get(type.getField("type").string()) != type.getField("size").number().intValue())
-				throw new ParseException("Type "+type.getField("type").string()+" is inconsistent with type assumptions.",0);
+			else if(Type.TYPE_SIZE_ASSUMPTIONS.get(type.getString("type")) == null)
+				throw new ParseException("Type "+type.getString("type")+" is inconsistent with type assumptions.",0);
+			else if(Type.TYPE_SIZE_ASSUMPTIONS.get(type.getString("type")) != type.getInt("size"))
+				throw new ParseException("Type "+type.getString("type")+" is inconsistent with type assumptions.",0);
+		}
 	}
 	/*
 	 * This function converts JSONField object to more relevant data structure
 	 */
-	private void translatePacketDefinitions(JSONField packages) throws ParseException
+	private void translatePacketDefinitions(JSONArray packages) throws ParseException
 	{
 		//translate configuration definition to easier data structure in PACKET_DEFINITIONS
-		for(JSONField packetPackage: packages)
+		for(Object packetPackageObject: packages)
 		{
 			//package
+			JSONObject packetPackage = (JSONObject)packetPackageObject;
 			HashMap<String, List<PacketDataSlot>> _package = new HashMap<String, List<PacketDataSlot>>(); 
-			PACKET_DEFINITIONS.put(packetPackage.getField("package").string(), _package);
-			for(JSONField packet: packetPackage.getField("packets"))
+			PACKET_DEFINITIONS.put(packetPackage.getString("package"), _package);
+			for(Object packetObject: packetPackage.getJSONArray("packets"))
 			{
 				//packet
+				JSONObject packet = (JSONObject)packetObject;
 				ArrayList<PacketDataSlot> slots = new ArrayList<PacketDataSlot>();
-				_package.put(packet.getField("packet").string(), slots);
-				for(JSONField dataSlot: packet.getField("definition"))
-					//data slot
+				_package.put(packet.getString("packet"), slots);
+				for(Object dataSlotObject: packet.getJSONArray("definition"))
+				{	//data slot
+					JSONObject dataSlot = (JSONObject)dataSlotObject;
 					slots.add(new PacketDataSlot
 					(
-						dataSlot.getField("name").string(),
-						Type.valueOf(dataSlot.getField("type").string().toUpperCase()),
-						dataSlot.getField("value") != null?dataSlot.getField("value").getObject():null
+						dataSlot.getString("name"),
+						Type.valueOf(dataSlot.getString("type").toUpperCase()),
+						dataSlot.get("value") != JSONObject.NULL?dataSlot.get("value"):null //TODO: check if dataSlot.get("value") causes error
 					));
+				}
 				if((!slots.get(0).getName().contentEquals("HEADER")) || slots.get(0).getType() != Type.BYTE)
 					throw new ParseException("Packets must start with header byte to identify packet to reciever.",0);
 			}
